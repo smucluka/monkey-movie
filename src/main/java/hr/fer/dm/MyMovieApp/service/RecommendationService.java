6e116e5177fbab48031606a3077370e30a834da1
smucluka <lukasimunec96@gmail.com>
2020-01-09 18:59:38 +0100

    Update

--- src/main/java/hr/fer/dm/MyMovieApp/service/RecommendationService.java
+++ src/main/java/hr/fer/dm/MyMovieApp/service/RecommendationService.java
@@ -211,7 +211,7 @@ public class RecommendationService {
 					if(mov.getTitle() == null || mov.getTitle() == "") continue;
 				}
 				
-				double value = ((double) entry.getValue()*10) + (double) calculateBonus(genreBonusMap, mov.getGenres());
+				double value = ((double) entry.getValue()*16) + (double) calculateBonus(genreBonusMap, mov.getGenres());
 
 				//OUTLIER GENERS!!!
 				if(mov.getGenres().contains("Animation")) {
@@ -308,7 +308,7 @@ public class RecommendationService {
 	}
 	
 	public Double calculateBonus(HashMap<String, Double> bonusMap, String genres){
-		double valuePerGenre = 50.0/genres.split("\\|").length;
+		double valuePerGenre = 20.0/genres.split("\\|").length;
 		double bonusSum = 0;
 		for(String gen : genres.split("\\|")) {
 			if(bonusMap.containsKey(gen)) {
