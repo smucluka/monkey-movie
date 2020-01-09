27d1fd127d0f9ce622b822e6e3d6da8dc17269a6
smucluka <lukasimunec96@gmail.com>
2020-01-09 16:58:05 +0100

    Promjene

--- src/main/java/hr/fer/dm/MyMovieApp/service/RecommendationService.java
+++ src/main/java/hr/fer/dm/MyMovieApp/service/RecommendationService.java
@@ -138,7 +138,7 @@ public class RecommendationService {
 
 		Map<Long, List<Ratings>> usersMap = new HashMap<Long, List<Ratings>>();
 		
-		for (int i=0; i<150; i++) {
+		for (int i=0; i<250; i++) {
 			if(added.size() == 0) break;
 			int index = random.nextInt(added.size());
 			usersMap.put(added.get(index), ratingsRepository.findByUserId(added.get(index)));
@@ -185,7 +185,7 @@ public class RecommendationService {
 		List<Movie> finalRecommendations = new ArrayList<Movie>();
 		int i = 0;
 		DecimalFormat df = new DecimalFormat("#.##");
-		while (entries.hasNext() && i < NUM_RECOMMENDATIONS + 4) {
+		while (entries.hasNext() && i < NUM_RECOMMENDATIONS + 3) {
 			Map.Entry entry = (Map.Entry) entries.next();
 			if ((double) entry.getValue() >= MIN_VALUE_RECOMMENDATION) {
 				List<Movie> moviesList = movieRepository.findByMovieId(Long.valueOf("" + entry.getKey()));
