926e948fa733359cd294bed3d977fc206ea82b2b
smucluka <lukasimunec96@gmail.com>
2020-01-09 20:37:18 +0100

    Update

--- src/main/java/hr/fer/dm/MyMovieApp/service/RecommendationService.java
+++ src/main/java/hr/fer/dm/MyMovieApp/service/RecommendationService.java
@@ -313,7 +313,7 @@ public class RecommendationService {
 		for(String gen : genres.split("\\|")) {
 			if(bonusMap.containsKey(gen)) {
 				Double percentage = bonusMap.get(gen);
-				bonusSum += (percentage * valuePerGenre * 1.3);
+				bonusSum += (percentage * valuePerGenre * 1.5);
 			}
 		}
 		return bonusSum;
